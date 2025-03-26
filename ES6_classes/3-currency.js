6713fdda2789495c760de9dbfde2b494b88e4ac1
meurtphy <erwanmpn5@gmail.com>
2025-03-26 18:33:56 +0100

    point manquant malgre les bonne sortie qui on des erreur mais la bonne finalité

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,14 +1,7 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
 
   // --- Getters ---
@@ -35,7 +28,7 @@ export default class Currency {
     this._name = value;
   }
 
-  // --- Method ---
+  // --- Méthode demandée ---
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
