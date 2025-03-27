21125a9d94156f07062dd271801a371c3adb0e2f
meurtphy <erwanmpn5@gmail.com>
2025-03-27 10:03:12 +0100

    refix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,34 +1,30 @@
 export default class Currency {
-    constructor(code, name) {
-      // Utilisation des setters pour valider et assigner les valeurs
-      this.code = code;
-      this.name = name;
-    }
-
-    // Getter et setter pour "code"
-    get code() {
-      return this._code;
-    }
-    set code(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      this._code = value;
-    }
-
-    // Getter et setter pour "name"
-    get name() {
-      return this._name;
-    }
-    set name(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = value;
-    }
-
-    // Méthode qui affiche "name (code)"
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+  constructor(code, name) {
+    // Store attributes with underscore prefix
+    this._code = code;
+    this._name = name;
   }
+
+  // Getter and setter for 'code'
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  // Getter and setter for 'name'
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  // Method to display full currency in the format name (code)
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,24 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-
     this._sqft = sqft;
-
-    // Vérifie si la méthode est bien redéfinie dans une sous-classe
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+    // Ensure that subclasses implement evacuationWarningMessage
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
-  // Getter
+  // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  // Méthode abstraite (doit être redéfinie dans les sous-classes)
-  evacuationWarningMessage() {
-    throw new Error(`Class ${this.constructor.name} must override evacuationWarningMessage`);
   }
 }
