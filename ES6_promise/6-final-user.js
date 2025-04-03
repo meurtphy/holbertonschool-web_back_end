9e9edec456c3927a10d5accd26de46328afdb302
meurtphy <erwanmpn5@gmail.com>
2025-04-03 14:01:35 +0200

    arl

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
-
-  const results = await Promise.allSettled(promises);
-
-  return results.map((result) => ({
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promesse = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promesse).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.value || result.reason,
-  }));
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  })));
 }
