4c3363b6b03e2c62f4ed95e556c973f9572ffccd
meurtphy <erwanmpn5@gmail.com>
2025-04-24 09:53:47 +0200

    all task woeking

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,7 @@ Module that defines task_wait_random function to create asyncio Tasks.
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Creates and returns an asyncio.Task for wait_random with max_delay.
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,6 +8,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns task_wait_random n times and returns the list of delays
