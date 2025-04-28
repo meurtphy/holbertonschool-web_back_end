2a33bbd0d35d0abb44602d313f9c2ee7e3c28964
meurtphy <erwanmpn5@gmail.com>
2025-04-28 12:50:49 +0200

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+"""Simple """
 import asyncio
 import random
-
+from typing import Generator
 
 async def async_generator():
     """Coroutine qui génère 10 nombres aléatoires"""
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Simple """
+
 import asyncio
 from typing import List
 
