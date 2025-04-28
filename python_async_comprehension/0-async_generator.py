9b6a5432f0e9504093cb73904dd3582159856be6
meurtphy <erwanmpn5@gmail.com>
2025-04-28 15:40:02 +0200

    refix dogstring

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,6 +5,8 @@ Module for async generator 2.0
 import asyncio
 import random
 from typing import Generator
+
+
 async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that loops 10 times
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-"""Measure the total runtime of executing async_comprehension 4 times in parallel."""
+"""
+Measure the total runtime of executing async_comprehension
+4 times in parallel.
+"""
 
 import asyncio
 import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
-    """Execute async_comprehension four times in parallel and measure the total runtime."""
+    """
+    Execute async_comprehension four times in parallel
+    and measure the total runtime.
+    """
     start_time = time.perf_counter()
 
     # Create a list of coroutines
