d8161c50d49498893da60e9548c51ab32f733e39
meurtphy <erwanmpn5@gmail.com>
2025-04-28 12:57:20 +0200

    fix docstring 1

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Simple """
+"""0-async_generator.py"""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
-async def async_generator():
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine qui génère 10 nombres aléatoires"""
     for _ in range(10):
         await asyncio.sleep(1)
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Simple """
+
 import asyncio
 import time
 
