3158057b63a57b8ac59d8ecf4c7b9d9ef5f09123
meurtphy <erwanmpn5@gmail.com>
2025-04-28 14:11:17 +0200

    refix 4

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Coroutine that asynchronously generates random numbers."""
-
 import asyncio
 import random
 from typing import Generator, Any
 
 
-async def async_generator() -> Generator[float, None, None]: #type: ignore
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """Generate 10 random numbers between 0 and 10, waiting 1 second between each."""
     for _ in range(10):
         await asyncio.sleep(1)
