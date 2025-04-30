4edf3f04acc999c605b7216d71a67878e9506ebb
meurtphy <erwanmpn5@gmail.com>
2025-04-30 14:36:56 +0200

    clear pycodestyle

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -5,13 +5,17 @@ Deletion-resilient hypermedia pagination
 import csv
 import math
 from typing import List, Dict, Any
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
         self.__dataset = None
         self.__indexed_dataset = None
+
     def dataset(self) -> List[List]:
         """Cached dataset
         """
@@ -21,6 +25,7 @@ class Server:
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
         return self.__dataset
+
     def indexed_dataset(self) -> Dict[int, List]:
         """Dataset indexed by sorting position, starting at 0
         """
@@ -31,6 +36,7 @@ class Server:
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
+
     def get_hyper_index(
             self, index: int = None, page_size: int = 10
             ) -> Dict[str, Any]:
