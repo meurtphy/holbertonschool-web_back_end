f261f3c107f944fda1514eb6efbdb1c164e64717
meurtphy <erwanmpn5@gmail.com>
2025-05-20 13:50:09 +0200

    fix3

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.exit(); // 🔥 on termine le programme ici
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
-
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,27 +1,21 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (aPath) => {
   try {
-    const content = fs.readFileSync(path, 'utf-8');
-    const lines = content.split('\n').filter(line => line.trim() !== '');
-    const students = lines.slice(1); // en-tête ignoré
-
-    const fields = {};
-
-    for (const student of students) {
-      const [firstname, , , field] = student.split(',');
-      if (!fields[field]) fields[field] = [];
-      fields[field].push(firstname);
+    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
     }
-
-    console.log(`Number of students: ${students.length}`);
-    for (const field in fields) {
-      const list = fields[field].join(', ');
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${list}`);
+    for (const subject in subjects) {
+      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
     }
-  } catch (err) {
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
-}
-
+};
 module.exports = countStudents;
