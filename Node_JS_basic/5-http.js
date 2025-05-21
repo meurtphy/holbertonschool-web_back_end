e3f4e7685c07d0a9450a232117972069e465f2af
meurtphy <erwanmpn5@gmail.com>
2025-05-21 18:18:00 +0200

    23

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
-      if (err) reject(new Error('Cannot load the database'));
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
       const lines = data.trim().split('\n').filter((line) => line);
       const students = lines.slice(1);
       const fields = {};
@@ -25,20 +29,21 @@ function countStudents(path) {
   });
 }
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   const db = process.argv[2];
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(db)
-      .then((report) => {
-        res.end(`This is the list of our students\n${report}`);
-      })
-      .catch((err) => {
-        res.end(err.message);
-      });
+    try {
+      const report = await countStudents(db);
+      res.end(`This is the list of our students\n${report}`);
+    } catch (err) {
+      res.end('Cannot load the database');
+    }
+  } else {
+    res.end('Not found');
   }
 });
 
